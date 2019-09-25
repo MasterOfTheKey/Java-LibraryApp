838755c78bbcc56a2a52eb179b3ff31e387ae794
francesco <francesco@PC>
2019-09-25 23:22:03 +0200

    Commit Done
    Implemented partially command line GUI
    Still unimplemented txt archive

--- src/main/java/com/masterkey/library/core/Library.java
+++ src/main/java/com/masterkey/library/core/Library.java
@@ -67,29 +67,91 @@ public class Library {
     
     private void initializeAddBook() {
     	printDivider();
-    	System.out.print("NEW BOOK\nID >> ");
+    	System.out.print("NEW BOOK");
     	long id = getIdAsInput();
-    	System.out.print("TITLE >> ");
     	String title = getTitleAsInput();
-    	
+        double price = getPriceAsInput();
+        System.out.println("NEW AUTHOR");
+        ArrayList<Author> authors = getAuthorsAsInput();
+    }
+    private double getPriceAsInput(){
+        String priceAsString = "";
+        boolean done = false;
+        while(!done){
+            System.out.print("PRICE >> ");
+            priceAsString = keyboardScanner.nextLine();
+            if(inputIsBlank(priceAsString) || priceFormatIsCorrect(priceAsString) || isAlphabetic(priceAsString))
+                System.out.println("ERROR: price has been inserted wrongfully");
+            else done = true;
+        }
+        return Double.parseDouble(priceAsString);
+    }
+    
+    private ArrayList<Author> getAuthorsAsInput(){
+        boolean done = false;
+        String idAsString = "", name = "", surname = "";
+        ArrayList<Author> authors = new ArrayList<Author>();
+        while(!done){
+            boolean idDone = false;
+            while(!idDone){
+                System.out.print("ID >> ");
+                idAsString = keyboardScanner.nextLine();
+                if(inputIsBlank(idAsString) || isAlphabetic(idAsString)) 
+                    System.out.println("ERROR: ID has been inserted wrongfully");
+                else idDone = true;
+            }
+            boolean nameDone = false;
+            while(!nameDone){
+                System.out.print("name >> ");
+                name = keyboardScanner.nextLine();
+                if(inputIsBlank(name)) 
+                    System.out.println("ERROR: name has been inserted wrongfully");
+                else nameDone = true;
+            }
+            boolean surnameDone = false;
+            while(!surnameDone){
+                System.out.print("surname >> ");
+                surname = keyboardScanner.nextLine();
+                if(inputIsBlank(surname)) 
+                    System.out.println("ERROR: name has been inserted wrongfully");
+                else surnameDone = true;
+            }
+            boolean answerDone = false;
+            String answer = "";
+            while(answerDone){
+                System.out.print("add more authors Y/N >> ");
+                answer = keyboardScanner.nextLine();
+                if(!inputIsBlank(answer)) answerDone = true;
+                else System.out.println("ERROR: the answer must be Y or N");
+            }
+            if(isYes(answer)){
+                authors.add(new Author(Long.parseLong(idAsString),name,surname));
+            }
+        }
+        return authors;
     }
     private String getTitleAsInput() {
+        System.out.print("TITLE >> ");
     	return keyboardScanner.nextLine();
     }
     private long getIdAsInput() {
+        System.out.println("ID >>"); 
     	String idAsStr = "";
     	long result = 0;
     	boolean done = false;
     	while(!done) {
     		idAsStr = keyboardScanner.nextLine();
-        	if(inputIsNull(idAsStr) || isAlphabetic(idAsStr))System.out.print("ERROR: format not valid\n>> ");
+        	if(inputIsBlank(idAsStr) || isAlphabetic(idAsStr))System.out.println("ERROR: format not valid");
         	else {
         		result = Long.parseLong(idAsStr);
         	}
     	}
     	return result;
     }
-    private boolean inputIsNull(String input) {
+    private boolean isYes(String input){
+        return input.equals('Y');
+    }
+    private boolean inputIsBlank(String input) {
     	return (input.isBlank());
     }
     private boolean priceFormatIsCorrect(String input) {
